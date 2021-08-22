1ae09484ad31a481da412e446373434f7ca377a0
Matt Robertson <mattrobertson333@gmail.com>
2021-08-21 20:31:08 -0500

    Finished match view and navigation there

---
+++ PremierLeague/Model/MatchStatus.swift
@@ -0,0 +1,13 @@
+//
+//  MatchStatus.swift
+//  PremierLeague
+//
+//  Created by Matt Robertson on 8/21/21.
+//
+
+import Foundation
+
+enum MatchStatus {
+    case scheduled
+    case completed
+}
